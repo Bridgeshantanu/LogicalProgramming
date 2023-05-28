f8e5cf95345b8d99b712b0bf2f9ac169bef5e294
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-28 17:16:27 +0530

    Add: stopwatch program code is added

--- Program.cs
+++ Program.cs
@@ -19,6 +19,42 @@ namespace Logical_Programming
             ReverseNumber number = new ReverseNumber();
             number.GenerateReverseNumber();
 
+            StopwatchProgram stopwatch = new StopwatchProgram();
+
+            Console.WriteLine("Press 'S' to start the stopwatch.");
+            Console.WriteLine("Press 'E' to stop the stopwatch.");
+            Console.WriteLine("Press 'D' to display the elapsed time.");
+            Console.WriteLine("Press 'Q' to quit the program.");
+
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
+                switch (key.Key)
+                {
+                    case (ConsoleKey.S):
+                        stopwatch.Start();
+                        break;
+
+                    case (ConsoleKey.E):
+                        stopwatch.Stop();
+                        break;
+
+                    case (ConsoleKey.D):
+                        stopwatch.DisplayElapsedTime();
+                        break;
+
+                    case (ConsoleKey.Q):
+                        return;
+
+
+                    default:
+                        Console.WriteLine("Invalid key.");
+                        break;
+                }
+
+                Console.WriteLine();
+            }
 
 
         }
---
+++ StopwatchProgram.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programming
+{
+    internal class StopwatchProgram
+    {
+        public DateTime startTime;
+        public DateTime endTime;
+        public bool isRunning;
+
+        public void Start()
+        {
+
+            if (isRunning)
+            {
+                Console.WriteLine("Stopwatch is already running.");
+                return;
+            }
+
+            startTime = DateTime.Now;
+            isRunning = true;
+            Console.WriteLine("Stopwatch started.");
+        }
+
+        public void Stop()
+        {
+            if (!isRunning)
+            {
+                Console.WriteLine("Stopwatch is not running.");
+                return;
+            }
+
+            endTime = DateTime.Now;
+            isRunning = false;
+            Console.WriteLine("Stopwatch stopped.");
+        }
+
+        public void DisplayElapsedTime()
+        {
+            if (isRunning)
+            {
+                Console.WriteLine("Stopwatch is still running. Stop it to get the elapsed time.");
+                return;
+            }
+
+            TimeSpan elapsedTime = endTime - startTime;
+            Console.WriteLine("Elapsed time: " + elapsedTime);
+        }
+    }
+}
