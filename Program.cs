0cdc0e5b95ab1a0823664c7e0c844aee1ab67337
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-28 16:48:04 +0530

    Add: fibonacci series code is added

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+namespace Logical_Programming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FibonacciSeries series = new FibonacciSeries();
+            series.GenerateFibonacciSeries();
+
+        }
+    }
+    
+}
